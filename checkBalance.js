d924cb64c28bbf9314dffc98bae432b0ddc5038b
jaylee5253 <79701806+jaylee5253@users.noreply.github.com>
2022-10-09 16:26:40 +0900

    Update checkBalance.js

--- checkBalance.js
+++ checkBalance.js
@@ -5,7 +5,7 @@ const lcd = new LCDClient ({
 })
 async function checkBalance() {
     //const accAddress = "terra15526p9s6f0tz066wywpwrsmssnrl37lqjclhlg";
-    const accAddress = "terra1kslpg8tqm9eh57ysqj9vce70nm5nklsf6fe3tz";
+    const accAddress = "<WALLET_ADDRESS>";
     const jsonString = '{ "balance" : { "address" : "' + accAddress + '" }}';
     const query_msg = JSON.parse(jsonString);
     const tokenAddress = "terra1f0d9gat6sr5j5dv57w0lvvmcy6cdma7w895tf5";
